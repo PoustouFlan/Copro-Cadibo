ed95905101e60c56e2efcb2ae7df2fb0271e1897
Quentin Rataud <quentin.rataud@epita.fr>
2022-08-04 00:09:41 +0200

    bot_utils: add more platforms

--- bot_utils.py
+++ bot_utils.py
@@ -2,15 +2,31 @@ from secret import TOKEN
 from calendar_utils import timedelta_message
 
 ROLES = {
-    "codeforces": 835477747840974868,
-    "yukicoder": 837746728781611038,
-    "codejam": 835480582384910366,
     "atcoder": 837746179138912307,
+    "codechef": 835477747840974868,
+    "codeforces": 835477747840974868,
+    "codejam": 835480582384910366,
+    "codingame": 1004493089748033716,
+    "hackerrank": 1004493385396142110,
     "leetcode": 837745978378682448,
+    "yukicoder": 837746728781611038,
+
+    "botengineer": 1004491031628230756,
 }
 
 ADMIN = {
     427489795673948162,
+}
+
+CHANNELS = {
+    "atcoder": 1001140362577985546,
+    "codechef": 1004380321715847280,
+    "codeforces": 1001140349776953464,
+    "codingame": 1001140376901533796,
+    "codejam": 1001140387580235877,
+    "hackerrank": 1003419681450885312,
+    "leetcode": 1003419750350717079,
+    "topcoder": 1003419566917025814,
 }
 
 def mention_role(name):
@@ -28,23 +44,3 @@ def arrow_to_timestamp(arrow):
 def arrow_to_countdown(arrow):
     unix = int(arrow.timestamp())
     return f"<t:{unix}:R>"
-
-def event_message(event, contest):
-    name = event.name
-    begin = event.begin
-    duration = event.duration
-    description = event.description
-    duration_message = timedelta_message(duration)
-    if description != '':
-        description = ">>> " + description
-    
-    return (
-        f"───────────────────────────\n"
-        f"{mention_role(contest)}\n"
-        f"**{name}**\n\n"
-        f"{arrow_to_timestamp(begin)} ({arrow_to_countdown(begin)})\n"
-        f"{duration_message}\n"
-        f"{description}\n"
-    )
-
-
