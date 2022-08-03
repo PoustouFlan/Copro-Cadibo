1a12108ffeb917da0e84063912448b5e33e86c1e
Quentin Rataud <quentin.rataud@epita.fr>
2022-08-03 23:32:57 +0000

    calendar_utils: ignore past events

--- calendar_utils.py
+++ calendar_utils.py
@@ -13,11 +13,15 @@ def week_start():
     start = today - timedelta(days = today.weekday())
     return date_to_datetime(start)
 
+def week_now():
+    today = date.today()
+    return date_to_datetime(today)
+
 def week_end():
     return week_start() + timedelta(days = 7)
 
 def is_event_this_week(event):
-    return week_start() <= event.begin <= week_end()
+    return week_now() <= event.begin <= week_end()
 
 def week_events(contest):
     calendar = CALENDARS[contest]
