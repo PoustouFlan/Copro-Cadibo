4752af03413637fe097c927ca49f9fcc62965c7a
Quentin Rataud <quentin.rataud@epita.fr>
2022-08-04 08:17:32 +0000

    calendar_utils: test for future event

--- calendar_utils.py
+++ calendar_utils.py
@@ -2,31 +2,13 @@ from calendar_getter import *
 from datetime import datetime, date, timedelta
 from dateutil import tz
 
-def date_to_datetime(dt, hour = 0, minute = 0, second = 0):
-    timezone = tz.gettz('Europe/Paris')
-    return datetime(
-            dt.year, dt.month, dt.day, hour, minute, second, tzinfo =timezone
-    )
+def is_event_future(event):
+    return event.begin >= datetime.now(event.begin.tzinfo)
 
-def week_start():
-    today = date.today()
-    start = today - timedelta(days = today.weekday())
-    return date_to_datetime(start)
-
-def week_now():
-    today = date.today()
-    return date_to_datetime(today)
-
-def week_end():
-    return week_start() + timedelta(days = 7)
-
-def is_event_this_week(event):
-    return week_now() <= event.begin <= week_end()
-
-def week_events(contest):
+def future_events(contest):
     calendar = CALENDARS[contest]
     events = sorted(calendar.events)
-    events = filter(is_event_this_week, events)
+    events = filter(is_event_future, events)
     return events
 
 def timedelta_message(timedelta):
