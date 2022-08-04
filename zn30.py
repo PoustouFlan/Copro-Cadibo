aec637245e462ac5edf8b33c95d45fcf3c9a89c3
Quentin Rataud <quentin.rataud@epita.fr>
2022-08-04 08:58:13 +0000

    zn30: check for already existing events

--- zn30.py
+++ zn30.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 from bot_utils import *
 from calendar_utils import *
+from time import sleep as rompiche
 
 
 bot = commands.Bot(
@@ -10,23 +11,37 @@ bot = commands.Bot(
     intents = discord.Intents.all()
 )
 
+def scheduled_event_uids(guild):
+    uids = set()
+    for scheduled_event in guild.scheduled_events:
+        description = scheduled_event.description
+        uid = description.split("\nuid: ")[1]
+        uids.add(uid)
+    return uids
+
+def is_event_new(event, uids):
+    return event.uid not in uids
+
 @admin_only
 @bot.command()
 async def clear_scheduled_events(ctx):
     guild = ctx.guild
     for scheduled_event in guild.scheduled_events:
         await scheduled_event.delete()
+        rompiche(2)
 
-@admin_only
 @bot.command()
-async def future_contests(ctx, contest):
-    await ctx.message.delete()
+async def update_contest(ctx, contest):
+    debug(f"Updating contest {contest}...")
     events = future_events(contest)
+    uids = scheduled_event_uids(ctx.guild)
+    events = filter(lambda event: is_event_new(event, uids), events)
     for event in events:
         name = event.name
+        debug(f"Adding new event : {name}")
         begin = event.begin
         end = event.end
-        description = f"{event.description}\nuid: {event.uid}"
+        description = f"{event.description}uid: {event.uid}\n"
         location = description.split('\n')[0].split('Link: ')[1]
         await ctx.guild.create_scheduled_event(
             name = f"{DISPLAY[contest]} {name}",
@@ -35,5 +50,13 @@ async def future_contests(ctx, contest):
             description = description,
             location = location,
         )
+        rompiche(2)
+    debug("Complete.")
+
+@commands.max_concurrency(1, per=commands.BucketType.guild, wait=False)
+@bot.command()
+async def update(ctx):
+    for contest in CALENDARS:
+        await update_contest(ctx, contest)
 
 bot.run(TOKEN)
